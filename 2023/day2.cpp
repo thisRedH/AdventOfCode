f85bb4c1b05eaa6ab5acb35c39ec9a9f248549e0
thisRedH <thisRedH@users.noreply.github.com>
2023-12-02 20:23:52 +0100

    2B Finished

--- 2023/day2.cpp
+++ 2023/day2.cpp
@@ -43,13 +43,38 @@ uint32_t solve2a() {
 }
 
 
-
 uint32_t solve2b() {
     std::ifstream inputFile("./input/day2.txt");
+    
+    uint32_t sum = 0;
 
     for (std::string line; getline(inputFile, line);) {
+        line = line.substr(line.find(": ") +1);
+        std::replace(line.begin(), line.end(), ';', ',');
+        std::stringstream ssLine(line);
+
+        uint16_t redMin = 0, greenMin = 0, blueMin = 0;
+
+        for (std::string color; getline(ssLine, color, ',');) {
+            color.erase(0, color.find_first_not_of(" \t\n\r\f\v"));
+
+            uint16_t currentNum = 0;
+            for (char c : color) {
+                if (!std::isdigit(c)) break;
+                currentNum = (currentNum * 10) + (c - '0');
+            }
+
+            if (color.find("red") != std::string::npos) {
+                redMin = currentNum > redMin ? currentNum : redMin;
+            } else if (color.find("green") != std::string::npos) {
+                greenMin = currentNum > greenMin ? currentNum : greenMin;
+            } else if (color.find("blue") != std::string::npos) {
+                blueMin = currentNum > blueMin ? currentNum : blueMin;
+            }
+        }
+
+        sum += (redMin * greenMin * blueMin);
 
     }
-
-    return 0;
+    return sum;
 }
