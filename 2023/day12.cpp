7b87b0069531faf76208dbbe6aac7710f60f3a4e
thisRedH <thisRedH@users.noreply.github.com>
2023-12-15 13:32:39 +0100

    Rewritten 12A
    I needet to get a better understanding

--- 2023/day12.cpp
+++ 2023/day12.cpp
@@ -1,41 +1,89 @@
 
 #include "days.h"
 
-std::vector<uint32_t> memo;
+inline bool is_valid(const uint16_t& count, const uint16_t& gi, const std::vector<uint16_t>& groups) {
+    return
+        (gi == (groups.size() -1) && count == groups[gi]) ||
+        (gi == (groups.size()) && count == 0);
+}
+
+//#define DEBUG_GRAPH 1
+#ifdef DEBUG_GRAPH
 uint32_t valid_comb(
-    std::string& springs, std::vector<uint16_t>& groups,
+    const std::string& springs, const std::vector<uint16_t>& groups,
+    uint16_t si = 0, uint16_t gi = 0, uint16_t count = 0,
+    char last_c = '.', std::string deug_str = ""
+) {
+    uint32_t r = 0;
+    
+    deug_str += last_c;
+    
+    if (si < springs.size()) {
+        const char& c = springs[si++];
+    
+        if (count != 0) {
+            if (count > groups[gi]) return 0;
+        }
+
+#ifdef DEBUG_GRAPH_STEPPING
+        std::cout << "\x1b[90m" << deug_str << "\x1b[0m" << std::endl;
+#endif
+
+        if (c == '#' || c == '?') {
+            r += valid_comb(springs, groups, si, gi, count +1, '#', deug_str);
+        }
+
+        if (c == '.' || c == '?') {
+            if (gi < groups.size() && count == groups[gi]) {
+                r += valid_comb(springs, groups, si, gi +1, 0, '.', deug_str);
+            } else if (count == 0) {
+                r += valid_comb(springs, groups, si, gi,    0, '.', deug_str);
+            }
+        }
+    } else {
+        if (is_valid(count, gi, groups)) {
+            std::cout << "\x1b[97m" << deug_str << "\x1b[0m" << std::endl;
+            return 1;
+        } else {
+            std::cout << "\x1b[90m" << deug_str << "\x1b[0m" << std::endl;
+            return 0;
+        }
+    }
+    return r;
+}
+#else
+
+uint32_t valid_comb(
+    const std::string& springs, const std::vector<uint16_t>& groups,
     uint16_t si = 0, uint16_t gi = 0, uint16_t count = 0
 ) {
     uint32_t r = 0;
 
     if (si < springs.size()) {
-        const char& c = springs[si];
-        si++;
+        const char& c = springs[si++];
 
-        if (c == '?' || c == '.') {
-            if (c == '?') {
-                r += valid_comb(springs, groups, si, gi, count +1);
-            }
+        if (count != 0) {
+            if (count > groups[gi]) return 0;
+        }
 
-            if (gi < groups.size() && groups[gi] == count) {
-                // match, moving to next group
-                r += valid_comb(springs, groups, si, gi +1, 0);
-            } else if (count == 0) {
-                // no match, move to next character
-                r += valid_comb(springs, groups, si, gi, 0);
-            }
-        } else {
+        if (c == '#' || c == '?') {
             r += valid_comb(springs, groups, si, gi, count +1);
         }
+
+        if (c == '.' || c == '?') {
+            if (gi < groups.size() && count == groups[gi]) {
+                r += valid_comb(springs, groups, si, gi +1, 0);
+            } else if (count == 0) {
+                r += valid_comb(springs, groups, si, gi, 0);
+            }
+        }
     } else {
-        r = (gi == (groups.size() -1) && groups[gi] == count) ||
-            (gi == groups.size() && count == 0)
-            ? 1 : 0;
+        return is_valid(count, gi, groups) ? 1 : 0;
     }
 
     return r;
 }
-
+#endif //DEBUG_GRAPH
 
 uint32_t solve12a() {
     std::ifstream input("./input/day12.txt");
--- 2023/days.h
+++ 2023/days.h
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <numeric>
 #include <ranges>
+#include <chrono>
 
 #include <vector>
 #include <array>
