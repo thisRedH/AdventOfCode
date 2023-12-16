42e66e53da7dfbe739710bbe593a7e55a6eacc78
thisRedH <thisRedH@users.noreply.github.com>
2023-12-16 19:36:50 +0100

    13B Finished

--- 2023/day13.cpp
+++ 2023/day13.cpp
@@ -69,6 +69,77 @@ uint32_t solve13a() {
     return total;
 }
 
+
+uint32_t str_diff_count(const std::string& str, const std::string& other) {
+    uint32_t same_count =
+        std::inner_product(
+            str.begin(), str.end(),
+            other.begin(),
+            0,
+            std::plus<>(),
+            std::equal_to<>()
+        );
+
+    return std::max(str.size(),other.size()) - same_count;
+}
+
+uint16_t row_dirty_mirrored(const std::vector<std::string>& map) {
+    uint16_t row = 1;
+    uint8_t smudges = 0;
+    bool found = false;
+
+    for (row = 1; row < map.size() && !found; row++) {
+        for (uint16_t off = 0;; off++) {
+            if ((((row -1) - off) < 0 || (row + off) >= map.size())) {
+                if (smudges == 1)
+                    found = true;
+                break;
+            }
+
+            if (map[row -(off +1)] != map[row + off])
+                if (str_diff_count(map[row -(off +1)], map[row + off]) == 1) {
+                    smudges++;
+                } else {
+                    smudges = 0;
+                    break;
+                }
+        }
+
+    }
+
+    return (found && smudges == 1) ? row -1 : 0;
+}
+
+inline uint32_t dirty_mirrored(const std::vector<std::string>& map) {
+    return
+        (100 * row_dirty_mirrored(map))
+        + row_dirty_mirrored(transpose_vec_str(map));
+}
+
 uint32_t solve13b() {
-    return 69;
+    std::ifstream input("./input/day13.txt");
+
+    std::vector<std::vector<std::string>> mirror_maps;
+    uint32_t total = 0;
+
+    std::vector<std::string> current_map;
+    for (std::string line; std::getline(input, line);) {
+        if (line.empty()) {
+            if (!current_map.empty()) {
+                mirror_maps.push_back(std::move(current_map));
+                current_map.clear();
+            }
+        } else {
+            current_map.push_back(line);
+        }
+    }
+
+    if (!current_map.empty())
+        mirror_maps.push_back(std::move(current_map));
+
+    for (const auto& map : mirror_maps)
+        total += dirty_mirrored(map);
+
+    input.close();
+    return total;
 }
