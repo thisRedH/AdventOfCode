da5f21c895272f9d1021af1630bc858841c01e31
thisRedH <thisRedH@users.noreply.github.com>
2023-12-17 18:57:37 +0100

    Added day14 assignment

--- 2023/days.h
+++ 2023/days.h
@@ -20,7 +20,7 @@
 #include <map>
 #include <unordered_map>
 
-const uint16_t kCURRENT_DAY = 14;
+const uint16_t kCURRENT_DAY = 15;
 
 #define SHOW_AB(day,a,b)        (std::cout << "Day " << (day) <<\
                                  "\ta: " << (a) << '\n' <<\
@@ -41,6 +41,7 @@ uint32_t solve11a();            uint64_t solve11b();
 uint32_t solve12a();            uint64_t solve12b();
 uint32_t solve13a();            uint32_t solve13b();
 uint32_t solve14a();            uint64_t solve14b();
+uint32_t solve15a();            uint32_t solve15b();
 
 #ifdef IMPL_DAYS
 
@@ -87,6 +88,9 @@ void solve(uint16_t day) {
         break;
     case 14:
         SHOW_AB(day, solve14a(), solve14b());
+        break;
+    case 15:
+        SHOW_AB(day, solve15a(), solve15b());
         break;
     default:
         break;
