e19db9537e83e8cb7dd6385de28b744a1b15df4d
thisRedH <thisRedH@users.noreply.github.com>
2024-12-11 18:38:42 +0100

    10@24 Finished

--- 2024/days/day10.go
+++ 2024/days/day10.go
@@ -3,6 +3,8 @@ package days
 import (
     "fmt"
     "os"
+    "strings"
+    "unicode"
 )
 
 func Day10() (int64, int64) {
@@ -12,9 +14,57 @@ func Day10() (int64, int64) {
         return -1, -1
     }
 
-    var p1, p2 int64
+    var score, rating int64
+    var grid [][]int
+    var visited [][]bool
+    for _, l := range strings.Split(string(in), "\n") {
+        row := make([]int, 0, len(l))
+        for _, c := range l {
+            if unicode.IsDigit(c) {
+                row = append(row, int(c-'0'))
+            }
+        }
+        if len(row) > 0 {
+            grid = append(grid, row)
+            visited = append(visited, make([]bool, len(row)))
+        }
+    }
 
-    fmt.Println(in)
+    var traverse func(int, int, int, map[[2]int]struct{}) (int, int)
+    traverse = func(r, c, last int, dest map[[2]int]struct{}) (int, int) {
+        if !InBounds2D(grid, r, c) || visited[r][c] || grid[r][c] != last+1 {
+            return 0, 0
+        }
 
-    return p1, p2
+        if grid[r][c] == 9 {
+            if _, ok := dest[[2]int{r, c}]; ok {
+                return 0, 1
+            } else {
+                dest[[2]int{r, c}] = struct{}{}
+                return 1, 1
+            }
+        }
+
+        visited[r][c] = true
+        var ta, tb int
+        for _, d := range [][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
+            a, b := traverse(r+d[0], c+d[1], grid[r][c], dest)
+            ta += a
+            tb += b
+        }
+        visited[r][c] = false
+        return ta, tb
+    }
+
+    for r := range grid {
+        for c, v := range grid[r] {
+            if v == 0 {
+                a, b := traverse(r, c, -1, map[[2]int]struct{}{})
+                score += int64(a)
+                rating += int64(b)
+            }
+        }
+    }
+
+    return score, rating
 }
